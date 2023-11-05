8b65dab6b8bb0a8bfbc1ccbe40b13983d8cde65c
Cas <cssamonte@gmail.com>
2023-11-04 17:27:14 -0700

    Implemented pagination and details are rendering at bottom of app

--- parks-rec/frontend/src/components/App/index.jsx
+++ parks-rec/frontend/src/components/App/index.jsx
@@ -1,26 +1,30 @@
 import { useState, useEffect } from 'react';
 import Gallery from '../Gallery';
+import DetailsPage from '../DetailsPage'
 import './styles.css';
 
 function App() {
   const [parks, setParks] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1)
 
-// Define an async function to query the API & JSONify the response  
-async function getData(url) {
-    const res = await fetch(url)
-    const { data } = await res.json()
-    setParks(data)
-}
+  // Define an async function to query the API & JSONify the response  
+  async function getData(url) {
+      const res = await fetch(url)
+      const { data } = await res.json()
+      setParks(parks.concat(data))
+  }
 
-// Query the API on component mount, and get 40 Parks.
-useEffect(() => {
-    getData('https://developer.nps.gov/api/v1/parks?limit=50&start=0&api_key=UOdct2cZxW8G7nCXedCKcy7sofVSQiDbskbENcXg')
-}, [])
+  // Query the API on component mount, and get 40 Parks.
+  useEffect(() => {
+      getData('https://developer.nps.gov/api/v1/parks?limit=50&start=0&api_key=UOdct2cZxW8G7nCXedCKcy7sofVSQiDbskbENcXg')
+  }, [])
 
   return (
     <>
       <h1>Parks & Rec</h1>
-      <Gallery parks={parks} />
+      <Gallery parks={parks} refreshQueue={getData} />
+      {parks.length > 0 && <DetailsPage {...parks[0]} />}
+
     </>
   );
 }
--- parks-rec/frontend/src/components/Gallery/index.jsx
+++ parks-rec/frontend/src/components/Gallery/index.jsx
@@ -1,9 +1,47 @@
+import { useState } from 'react'
 import Card from '../Card'
 
-export default function Gallery({ parks }) {
+export default function Gallery({ parks, refreshQueue }) {
+    // Keep track of what gallery page the user is viewing
+    const [currentPage, setCurrentPage] = useState(1)
+
+    // Event handler for the next Page Button
+    function getNextPage() {
+        refreshQueue(`https://developer.nps.gov/api/v1/parks?limit=500&start=0&api_key=UOdct2cZxW8G7nCXedCKcy7sofVSQiDbskbENcXg&skip=${parks.length}`)
+        setCurrentPage(currentPage + 1)
+    }
+
+    // Event handler for the Previous Page button
+    function getPrevPage() {
+        setCurrentPage(currentPage - 1)
+    }
+
+
+    // The default value of gallery content. What we see before the app finsihes querying the API
+    let galleryContent = <p>National Parks are loading...</p>
+
+    // Conditionally update the gallery content depending on the current page
+    if (parks.length > 0 && currentPage > 1) {
+        const nextPage = currentPage + 1
+        galleryContent = parks
+            .slice(currentPage * 20, nextPage * 20) // get the 20 images of the array we want to see
+            .map((park, i) => <Card key={i} parkData={park} />) // map over the 20 images and render them in Card components
+    } else if (parks.length > 0 && currentPage === 1) {
+        galleryContent = parks
+            .slice(0, 20) // get the first 20 parks when on the first page
+            .map((park, i) => <Card key={i} parkData={park} />)
+    }
+
     return (
-        <div className="gallery">
-            {parks.length > 0 ? parks.map(park => <Card key={park.id} parkData={park} />) : <p>National Parks are loading...</p>}
-        </div>
+        <>
+            <div className="gallery">
+                {galleryContent}
+            </div>
+
+            <div className='page-controls'>
+                <button onClick={getPrevPage}>Previous Page</button>
+                <button onClick={getNextPage}>Next Page</button>
+            </div>
+        </>
     )
 }
