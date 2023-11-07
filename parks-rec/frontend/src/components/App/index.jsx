ff40c1413ce6f010039cf00e9260840ced92b5b9
Cas <cssamonte@gmail.com>
2023-11-06 22:00:20 -0800

    nav logo

--- parks-rec/frontend/src/components/App/index.jsx
+++ parks-rec/frontend/src/components/App/index.jsx
@@ -43,37 +43,36 @@ function App() {
 
   return (
     <>
-      {/* The Nav Bar */}
+      {/* Nav Bar */}
       <nav className="absolute top-0 left-0 w-full bg-transparent py-4 px-6 flex justify-between items-center z-10">
 
-      <div className="flex items-center"> 
-        <Link to="/">
-            <img src={whiteLogo} alt="Parks and Rec Logo" className="w-15 h-16" />
-        </Link>
-          <h2 className="text-lg font-bold text-white px-3">Parks & Rec</h2>
-      </div>
+        <div className="flex justify-between items-center w-full">
+          <div className="w-full absolute inset-0 flex justify-center pt-3 pointer-events-none">
+            <img src={whiteLogo} alt="Parks and Rec Logo" className="w-18 h-19 pointer-events-auto" />
+          </div>
 
-      <div className="flex items-center">
-        <Link to="/parks">
-          <button className="bg-white bg-opacity-50 text-gray-700 rounded-md py-2 px-4 ml-2 focus:outline-none">
-            US National Parks
-          </button>
-        </Link>
-        <Link to="/search">
-          <button className="bg-white bg-opacity-50 text-gray-700 rounded-md py-2 px-4 ml-2 focus:outline-none">
-            Search
-          </button>
-        </Link>
-      </div>
+            <Link to="/">
+              <h2 className="text-lg font-bold text-white px-3">Parks & Rec</h2>
+            </Link>
+        </div>
+         
+          <div className="flex items-center">
+            <Link to="/parks">
+              <button className="bg-white bg-opacity-50 text-gray-700 rounded-md py-2 px-2 ml-2 focus:outline-none min-w-max">
+                US National Parks
+              </button>
+            </Link>
+            <Link to="/search">
+              <button className="bg-white bg-opacity-50 text-gray-700 rounded-md py-2 px-4 ml-2 focus:outline-none">
+                Search
+              </button>
+            </Link>
+        </div>
 
       </nav>
 
       {/* Routes */}
-      <Routes>
-          <Route path="/" element={
-            <HomePage news={news} />
-          }
-        />
+      <Routes>        
         <Route path="/parks" element={
           <ParksPage
             parks={parks}
@@ -81,6 +80,7 @@ function App() {
             setDetailsData={setDetailsData}
           />}
         />
+        <Route path="/" element={ <HomePage news={news} />} />
         <Route path="/search" element={<SearchPage setDetailsData={setDetailsData} />} />
         <Route path="/details" element={<DetailsPage {...detailsData} />} />
       </Routes>
